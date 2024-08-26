91e90d13f7df4429d31f90f00f833762de0cad9b
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-26 07:33:05 -0900

    old way of fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("http://localhost:8080/todos")
+        .then((res) => setData(res.data))
+        .catch((err) => console.log(err));
+    };
+    fetchData()
+  });
+
+  console.log(data)
 
   return (
-    <div className="" >
-      
+    <div>
+      {JSON.stringify(data)}
     </div>
   )
 }
 
-export default App
+export default App;
