34e342322e8b93576a82c5fc752b3caf2b8992fd
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-26 07:25:42 -0900

    setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "./components/ui/button"
-
 
 function App() {
 
   return (
-    <div className="text-2xl text-rose-500" >
-      Hello World 
-      <Button>Click Me</Button>
+    <div className="" >
+      
     </div>
   )
 }
